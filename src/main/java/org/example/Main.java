566f3ccecf4cf639346439d099558c5cc6a0b649
GauharAmanbay <gamanbajkyzy@gmail.com>
2023-10-22 12:36:46 +0500

    some changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,11 +4,17 @@ public class Main {
     public static void main(String[] args) {
         mainMethodShowMessageAble();
         mainMethodShowInfo();
+        mainMethodShowSteps();
     }
     private static void mainMethodShowMessageAble() {
         System.out.println("GitLesson");
     }
     private static void mainMethodShowInfo() {
         System.out.println("Learning how use Git");
+    }
+    private static void mainMethodShowSteps() {
+        System.out.println("1.Go to the site github.com");
+        System.out.println("2.Sign up");
+        System.out.println("3.Now you can use your GitAccount");
     }
 }
